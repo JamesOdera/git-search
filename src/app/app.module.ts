a678f6eee85a1ba0ef9667de71aaf9b2c43ef840
JamesOdera <nyangoma.odera@gmail.com>
2020-01-17 17:14:50 +0300

    add https client service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { ProfileService } from './profile.service';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
@@ -16,7 +18,7 @@ import { ProfileComponent } from './profile/profile.component';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [ProfileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/http';
-import 'rxjs/add/operator/map';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +11,13 @@ export class ProfileService {
   private clientid:'088761f8956b9ece1dbb';
   private clientsecret:'9f9f07b89d82b755d55281498cf9279ccc03d0a4';
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) {
+    console.log("service is now ready!");
+    this.username = 'JamesOdera';
+   }
+
+   getProfileInfo(){
+     return this.http.get("https://api.github.com/users/" + this.username + "?client id=" + this.clientid + "&client_secret=" + this.clientsecret)
+    //  .map(res => res.json());
+   };
 }
