9e9c9f28a3979b81c421881925c879e2340fa3fc
JamesOdera <nyangoma.odera@gmail.com>
2020-01-17 15:18:44 +0300

    add a service folder and http client service

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  private username: string;
+  private clientid:'088761f8956b9ece1dbb';
+  private clientsecret:'9f9f07b89d82b755d55281498cf9279ccc03d0a4';
+
+  constructor(private http:HttpClient) { }
 }
