c5bd267e7e3289081ccbb9d115b2fa01bced6f09
JamesOdera <nyangoma.odera@gmail.com>
2020-01-18 14:21:21 +0300

    add styles to the profile

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,33 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+// import { Observable } from 'rxjs/Observable';
+// import 'rxjs/add/operators/map';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  private username: string;
+  private userName: string;
   private clientid:'088761f8956b9ece1dbb';
   private clientsecret:'9f9f07b89d82b755d55281498cf9279ccc03d0a4';
 
   constructor(private http:HttpClient) {
     console.log("service is now ready!");
-    this.username = 'JamesOdera';
+    this.userName = 'JamesOdera';
    }
 
    getProfileInfo(){
-     return this.http.get("https://api.github.com/users/" + this.username + "?client id=" + this.clientid + "&client_secret=" + this.clientsecret)
-    //  .map(res => res.json());
+     return this.http.get("https://api.github.com/users/" + this.userName + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+      // .map(res => res.json())
+      // // .catch(this.handleError);
    };
 
    getProfileRepos(){
-    return this.http.get("https://api.github.com/users/" + this.username + "/repos?client id=" + this.clientid + "&client_secret=" + this.clientsecret)
+    return this.http.get("https://api.github.com/users/" + this.userName + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
    }
 
 
-   updateProfile(username: string){
-     this.username = username;
+   updateProfile(userName: string){
+     this.userName = userName;
    }
 
 
